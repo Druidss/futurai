4e69da4deaf310af4320d8db7bd4146f1462a3f6
Yuxuan Yang <yxyn4921@hochschule-trier.de>
2024-08-15 18:23:30 +0200

    add gsap add parallax animaton

--- one-page/src/components/Page1.vue
+++ one-page/src/components/Page1.vue
@@ -1,6 +1,8 @@
 <template>
-  <ScrollView root :duration="1.7">
-    <div class="h-screen w-screen mx-auto  flex justify-center items-center  bg-image bg-cover bg-center">
+  <main ref="container" data-scroll-container>
+    <!-- futurAI -->
+    <div class="h-screen w-screen mx-auto  flex justify-center items-center  bg-image bg-cover bg-center"
+      data-scroll-section data-scroll data-scroll-sticky>
       <div class="text-ceter">
         <div class="splt text-9xl font-bold font-pixel tracking-widest">FUTUR AI</div>
         <div class="splt text-xl max-auto justify-center text-center">
@@ -8,70 +10,102 @@
         </div>
       </div>
     </div>
-  </ScrollView>
 
-  <div class="h-screen w-screen flex jusify-center items-center bg-white">
-    <div class="text-center w-9/12 mx-auto">
-      <ScrollView root :duration="1.7">
-        <ScrollComponent class="page2-init leading-tight text-9xl font-pixel text-black mx-auto my-2 max-w-9/12"
-          data-scroll-repeat="true" data-scroll-class="page2-opacity" :speed='0.2' cssProgress>
+
+    <div class="h-screen w-screen flex jusify-center items-center bg-white" data-scroll-section>
+      <div class="text-center w-9/12 mx-auto mt-8">
+        <ScrollComponent class="page2-init leading-none text-9xl font-pixel text-black mx-auto my-2 max-w-9/12"
+          data-scroll data-scroll-repeat="true" :speed="0.2" data-scroll-class="page2-opacity">
           A game using
           dice and map
-          for ONE player about future AI</ScrollComponent>
-      </ScrollView>
-    </div>
-  </div>
-
-  <ScrollView root :duration="1.7">
-    <div class="h-screen w-screen flex jusify-center items-center bg-black">
-      <div class="text-center w-9/12 mx-auto overflow-hiden section3" id="3-section" data-scroll-section>
-        <ScrollComponent class="text-9xl section3-one font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-2"
-          :speed='6' data-scroll-direction="horizontal">
-          <span>GET YOUR IDENTITY REPORT GET YOUR IDENTITY REPORT</span>
-        </ScrollComponent>
-        <ScrollComponent class="text-9xl section3-two font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-2"
-          :speed='-6' data-scroll-direction="horizontal">
-          <span>GET YOUR IDENTITY REPORT GET YOUR IDENTITY REPORT</span>
-        </ScrollComponent>
-        <ScrollComponent class="text-9xl section3-three font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-2"
-          :speed='6' data-scroll-direction="horizontal">
-          <span>GET YOUR IDENTITY REPORT GET YOUR IDENTITY REPORT</span>
-        </ScrollComponent>
-        <ScrollComponent class="text-9xl section3-four font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-2"
-          :speed='6' data-scroll-direction="horizontal">
-          <span>GET YOUR IDENTITY REPORT GET YOUR IDENTITY REPORT</span>
+          for ONE player about future AI
         </ScrollComponent>
       </div>
     </div>
-  </ScrollView>
 
-  <ScrollView root :duration="1.7">
-    <div class="h-screen w-screen flex jusify-center items-center bg-white">
-      <ScrollComponent class="text-9xl text-yellow-900" data-scroll data-scroll-direction="horizontal"
-        data-scroll-speed="2" data-scroll-delay="0.05"> my idea
-      </ScrollComponent>
-      <div class="text-9xl text-yellow-900" data-scroll data-scroll-direction="horizontal" data-scroll-speed="2"
-        data-scroll-delay="0.05"> my idea</div>
-      <div class="text-9xl text-yellow-900" data-scroll data-scroll-direction="horizontal" data-scroll-speed="2"
-        data-scroll-delay="0.05"> my idea</div>
+    <!-- Feature -->
+    <div class="h-screen w-screen bg-black overflow-hidden" data-scroll-section>
+      <div class="text-9xl section3-one font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-4">
+        <span>GET YOUR IDENTITY REPORT GET YOUR IDENTITY REPORT</span>
+      </div>
+      <div class="text-9xl section3-two font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-4"
+        data-scroll-speed='-6' data-scroll-direction="horizontal">
+        <span> 36 DIFFEERENT EVENTS FOR FURTUR </span>
+      </div>
+      <div class="z-40 text-9xl section3-three font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-4"
+        data-scroll-direction="horizontal">
+        <span class="bg-white p-4 ">Make your own choices</span>
+      </div>
+      <div class=" z-50 text-9xl section3-four font-pixel text-nowrap w-screen h-1/12 bg-white text-black p-4"
+        data-scroll-direction="horizontal">
+        <span class="bg-white py-4 ">FORSEE FOR FUTURE FORSEE FOR FUTURE</span>
+      </div>
     </div>
-  </ScrollView>
 
+    <!-- Brief -->
+    <div class="h-screen w-screen  overflow-hidden bg-white" data-scroll-section>
+      <div class="flex justify-between  w-9/12 mx-auto">
+        <div class="text-9xl my-36 text-black text-nowrap font-pixel" data-scroll-speed="0.1" data-scroll-sticky
+          data-scroll-repeat data-scroll-delay="0.2">
+          BRIEF
+        </div>
+
+        <div class="max-w-md my-36 mx-12 text-black brief-text" data-scroll data-scroll-speed="0.3" data-scroll-repeat
+          data-scroll-class="page2-opacity">
+          FuturAI is an innovative online card game set in a speculative future where algorithms dictate every aspect of
+          life, from careers and relationships to leisure activities. The game incorporates elements of speculative
+          design, using a four-quadrant method to predict and explore potential futures. </br>
+
+          As players navigate through the
+          game, they confront the implications of algorithmic control, making strategic decisions that affect not only
+          their in-game progress but also their understanding of a future shaped by technology and data-driven
+          insights..
+        </div>
+      </div>
+    </div>
+
+    <!-- Future quadrants -->
+    <div class="h-screen w-screen  overflow-hidden bg-black" data-scroll-section>
+      <div class="flex justify-between w-9/12 mx-auto">
+        <div class="mx-24 my-36 text-white" data-scroll data-scroll-speed="0.1" data-scroll-sticky data-scroll-repeat>
+          <div class="text-4xl text-nowrap font-pixel"> Future quadrants </div>
+          <div class="my-8 max-w-md"> To help us understand our world, we built an immersive experience that allows us
+            to
+            travel through different environments where we might live in someday. We created four scenario that each
+            present an alternative view of future worlds.</div>
+        </div>
+
+        <div class="max-w-screen-xlmy-12  text-white" data-scroll data-scroll-speed="0.4" data-scroll-repeat>
+          <img class="h-4/12 my-36" :src="four"> </img>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 import splt from 'spltjs';
 import anime from 'animejs/lib/anime.es.js'; 
-import { ScrollView, ScrollComponent } from 'potiah'
+import LocomotiveScroll from "locomotive-scroll";
+import { ScrollView, ScrollComponent } from 'potiah';
+import { gsap } from 'gsap';
+import four from '../assets/four.png';
 
 export default {
   components: {
     ScrollView,
     ScrollComponent,
   }, 
+  data() {
+    return {
+      four: four,
+    }
+  },
   mounted() {
     splt({});
-    this.anime();
+    this.anime(); 
+    this.setLocomotiveScroll();
+    this.directionScroll();
   },
   methods: {
     anime:() => {
@@ -93,17 +127,37 @@ export default {
           delay: 2000,
         })
     },
+    setLocomotiveScroll() {
+      new LocomotiveScroll({
+        el: this.$refs.container,
+        smooth:true,
+        multiplier: 1,
+        direction: 'horizontal',
+        repeat:true,
+        horizontal: true,
+      });
+    },
+    directionScroll(){
+      const sections = gsap.utils.toArray('.section3-one, .section3-two, .section3-three, .section3-four');
+      const tl = gsap.timeline({repeat: -1, repeatDelay: 0.5});
+      tl
+        .add('start')
+        .to(sections[0], { duration: 4, x: 800, y: 400, ease: 'power2.inOut' }, 'start')
+        .to(sections[1], { duration: 2, x: 200, y:-100, ease: 'power2.inOut' }, 'start+=0.5')
+        .to(sections[2], { duration: 6, x: 300, y: 200, ease: 'power2.inOut' }, 'start+=1')
+        .to(sections[3], { duration: 2, x: -400, ease: 'power2.inOut' }, 'start+=1.5');
+
+        // tl.revert();
+    }
   },
 }
 </script>
 
 <style>
-.page2-init{
-  opacity: 0;
-  transition: opactiy 6s;
-}
+
 .page2-opacity {
   opacity: 1;
+  transition: opactiy 6s;
 }
 
 .section3 span {
@@ -115,10 +169,11 @@ export default {
 
 .section3-one{
     top: 33%;
-    transform: translateX(-50vw) rotate(26deg)
+    transform: translateX(-50vw) rotate(36deg)
 }
 .section3-two{
-  top: 45%
+  top: 45%;
+  transform: translateX(50vw) rotate(6deg)
 }
 .section3-three{
       top: 55%;
@@ -130,6 +185,6 @@ export default {
 }
 .section-five{
       top: 63%;
-        transform: translateX(-10vw) rotate(3deg)
+      transform: translateX(-10vw) rotate(3deg)
 }
 </style>
--- one-page/src/main.js
+++ one-page/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-// import 'locomotive-scroll/locomotive-scroll.css' 
 import { createPotiah } from 'potiah'
 
 createApp(App).use(createPotiah()).mount('#app')
